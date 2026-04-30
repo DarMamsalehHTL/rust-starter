17b811e90f5b24f160cfbbc5ea62cddb80ffaa19
DarMamsalehHTL <d.mamsaleh@students.htl-leonding.ac.at>
2026-04-30 13:35:08 +0200

    added add_task to program. Still bugs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    println!("What do you want to do? (1) add task, (2) view tasks, (3) mark task as done, (4) delete task");
+    let read: String = io::stdin().read_line(&mut String::new()).unwrap().to_string();
+    let read: i32 = read.trim().parse().unwrap();
+    if read == 1
+    {
+        add_task();
+    }
+}
+
+fn add_task() {
+    println!("Enter task name:");
+    let _read = io::stdin().read_line(&mut String::new()).unwrap();
+    
+
 }
